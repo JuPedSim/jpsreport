b3e1d5354cc46baa8f3f9b6805ec2f2ef137b6a6
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2018-11-09 18:00:50 +0100

    missing apostroph

--- methods/Method_A.cpp
+++ methods/Method_A.cpp
@@ -124,7 +124,7 @@ void Method_A::WriteFile_N_t(string data)
           string file_N_t ="Flow_NT_"+_trajName+"_id_"+_measureAreaId+".dat";
           if(_plotTimeSeries)
           {
-               string parameters_N_t=" " + _scriptsLocation+"/_Plot_N_t.py\" -p \""+ METHOD_A_LOCATION + "\" -n "+file_N_t;
+               string parameters_N_t=" " + _scriptsLocation+"/_Plot_N_t.py -p  \""+ METHOD_A_LOCATION + "\" -n "+file_N_t;
                parameters_N_t = PYTHON + parameters_N_t;
                std::cout  << ">> <"<< parameters_N_t << ">\n";
 

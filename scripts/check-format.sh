ecd62d04b12b47657b71d31138e10e43bff08037
schroedtert <t.schroedter@fz-juelich.de>
2022-01-07 14:48:47 +0100

    Correct CI to run check format on ubuntu and macos

--- scripts/check-format.sh
+++ scripts/check-format.sh
@@ -1,11 +1,19 @@
 #! /bin/bash
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    CPUS=$(nproc)
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    CPUS=$(sysctl -n hw.logicalcpu)
+fi
 
 location="$(cd "$(dirname "${0}")";pwd -P)"
-root=$(readlink -f "${location}/..")
+root=$( cd "$(dirname "${location}")" ; pwd -P )
+
+echo ${location}
+echo ${root}
 
 return_code=0
 
-isort --check --jobs "$(nproc)" "${root}" || return_code=1
+isort --check --jobs "${CPUS}" "${root}" || return_code=1
 black --check "${root}" || return_code=1
 
 exit ${return_code}
--- scripts/format.sh
+++ scripts/format.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
-
 set -e
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    CPUS=$(nproc)
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    CPUS=$(sysctl -n hw.logicalcpu)
+fi
 
 location="$(cd "$(dirname "${0}")";pwd -P)"
-root=$(readlink -f "${location}"/..)
-isort --jobs "$(nproc)" "${root}"
+root=$( cd "$(dirname "${location}")" ; pwd -P )
+
+isort --jobs "${CPUS}" "${root}"
 black "${root}"
